d2d163e9406a72f9cacc126584f8ad7ac74d17d0
Javier Marquez <javi@arqex.com>
2023-10-18 21:59:33 +0200

    Adds some logging to the test setup

--- scripts/dev-tests-setup.sh
+++ scripts/dev-tests-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "Generating templates..."
 node scripts/generate-templates.js
+echo "Creating testing widget..."
 npx create-react-widget test-widget -y
 cd test-widget
+echo "Running vite"
 npm run dev
